912793d97f0f747227bc72efdcf103d00db6d854
Doveccl <doveccl@live.com>
2018-05-04 16:47:19 +0800

    fix test script

--- test.js
+++ test.js
@@ -7,8 +7,8 @@ const ord = c => c.charCodeAt(0)
 const str = 'Hello DCrypto!'
 
 for (let seed = 0; seed < 256; seed++) {
-    for (let a = ord('A'); a < ord('z'); a++)
-        for (let b = ord('A'); b < ord('z'); b++) {
+    for (let a = ord('A'); a <= ord('z'); a++)
+        for (let b = ord('A'); b <= ord('z'); b++) {
             let ab = chr(a) + chr(b)
             let enc = _.encrypt(str, seed, ab)
             let dec = _.decrypt(enc, seed, ab)
